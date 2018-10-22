d9c1e1f7dab7d9822d5330836a35ad71e547998e
Nick-Damico <nickalan82@icloud.com>
2018-10-21 21:07:46 -0400

    refactor .dinghy, .ship using arel_table method

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -8,10 +8,26 @@ class Boat < ActiveRecord::Base
   end
 
   def self.dinghy
-    self.where('length < ?', 20)
+    where(self.arel_table[:length].lt(20))
   end
 
   def self.ship
-    self.where('length >= ?', 20)
+    self.where(self.arel_table[:length].gteq(20))
+  end
+
+  def self.last_three_alphabetically
+    self.order(:name).reverse_order.limit(3)
+  end
+
+  def self.without_a_captain
+    self.where(:captain => nil)
+  end
+
+  def self.sailboats
+    self.includes(:classifications).where('classifications.name = "Sailboat"')
+  end
+
+  def self.with_three_classifications
+    # joins(:classifications).group('boat.id').having('count(classifications) > 2')
   end
 end
